bdeecfe427603e130d604a0484c9d18fc1a9a000
bytegen <studstud531@gmail.com>
2025-09-10 18:22:23 +0300

    Add Store class to store all kv pairs

---
+++ store.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"errors"
+	"sync"
+)
+
+var (
+	ErrKeyNotFound = errors.New("key not found")
+	ErrEmptyKey    = errors.New("key is empty")
+)
+
+type Store struct {
+	m map[string]string
+	sync.RWMutex
+}
+
+func NewStore() *Store {
+	return &Store{m: make(map[string]string)}
+}
+
+// helper to check if key is empty
+func (s *Store) isKeyValid(key string) error {
+	if key == "" {
+		return ErrEmptyKey
+	}
+	return nil
+}
+
+func (s *Store) Put(key string, value string) error {
+	if err := s.isKeyValid(key); err != nil {
+		return err
+	}
+
+	s.Lock()
+	defer s.Unlock()
+
+	s.m[key] = value
+	return nil
+}
+
+func (s *Store) Delete(key string) error {
+	if err := s.isKeyValid(key); err != nil {
+		return err
+	}
+
+	s.Lock()
+	defer s.Unlock()
+
+	delete(s.m, key)
+	return nil
+}
+
+func (s *Store) Get(key string) (string, error) {
+	if err := s.isKeyValid(key); err != nil {
+		return "", err
+	}
+
+	s.RLock()
+	defer s.RUnlock()
+
+	value, ok := s.m[key]
+	if !ok {
+		return "", ErrKeyNotFound
+	}
+	return value, nil
+}
