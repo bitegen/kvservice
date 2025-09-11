3167390c21d0ce6326c250bc5e7e5eda5cf3c287
bytegen <studstud531@gmail.com>
2025-09-11 17:34:52 +0300

    Add tests to check if storage file is created and concurrent writes are done correctly

---
+++ transaction/transaction_test.go
@@ -0,0 +1,95 @@
+package transaction
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"sync"
+	"testing"
+)
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func TestCreateTransactor(t *testing.T) {
+	const filename = "test_kv"
+	defer os.Remove(filename)
+
+	ctx := context.Background()
+	tr, err := NewTransactor(ctx, filename)
+	if err != nil {
+		t.Fatalf("cannot create transactor: %v", err)
+	}
+	defer tr.Close()
+
+	if tr == nil {
+		t.Fatal("transactor is nil")
+	}
+	if !fileExists(filename) {
+		t.Fatalf("file %s does not exist after creation", filename)
+	}
+}
+
+func TestConcurrentWritesAndRead(t *testing.T) {
+	ctx := context.Background()
+
+	transactor, err := NewTransactor(ctx, filename)
+	if err != nil {
+		t.Fatalf("failed to create transactor: %v", err)
+	}
+
+	defer func() {
+		if err := transactor.Close(); err != nil {
+			t.Fatalf("close error: %v", err)
+		}
+		os.Remove(transactor.file.Name())
+	}()
+
+	wg := &sync.WaitGroup{}
+	const workers = 5
+
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+
+			key := fmt.Sprintf("worker:%d", id)
+			val := fmt.Sprintf("value-%d", id)
+
+			if err := transactor.WritePut(ctx, key, val); err != nil {
+				t.Errorf("worker %d error: %v", id, err)
+			}
+		}(i)
+	}
+	wg.Wait()
+
+	transactor1, err := NewTransactor(ctx, filename)
+	if err != nil {
+		t.Fatalf("failed to create transactor1: %v", err)
+	}
+
+	defer func() {
+		if err := transactor1.Close(); err != nil {
+			t.Fatalf("close error: %v", err)
+		}
+		os.Remove(transactor.file.Name())
+	}()
+
+	var readEvents []Event
+	eventsCh, errCh := transactor1.readEvents()
+	for event := range eventsCh {
+		readEvents = append(readEvents, event)
+	}
+
+	if err := <-errCh; err != nil {
+		t.Fatalf("read error: %v", err)
+	}
+	if len(readEvents) != workers {
+		t.Fatalf("expected %d rows, got %d", workers, len(readEvents))
+	}
+}
